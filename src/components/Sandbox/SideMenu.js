fc12482e413723475318e55a7f464c77771ca944
13180599752@163.com <13180599752@163.com>
2022-08-31 08:29:55 +0800

    左侧sidemenu

--- src/components/Sandbox/SideMenu.js
+++ src/components/Sandbox/SideMenu.js
@@ -1,34 +1,79 @@
-import React,{useState} from 'react'
+import React from 'react'
+import { withRouter } from 'react-router-dom'
 import { Layout, Menu } from 'antd';
-import {UploadOutlined,UserOutlined,VideoCameraOutlined } from '@ant-design/icons';
+import {UploadOutlined,UserOutlined,VideoCameraOutlined,SettingOutlined } from '@ant-design/icons';
+import './index.css'
+
 const { Sider } = Layout;
-export default function SideMenu() {
-  const [collapsed, setCollapsed] = useState(false);
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+//模拟数组结构
+const menuItem=[
+  {
+    key:'/home',
+    title:'首页',
+    icon:<UserOutlined />
+  },
+  {
+    key:'/user-manage',
+    title:'用户管理',
+    icon:<UserOutlined />,
+    children:[
+      {
+        key:'/user-manage/list',
+        title:'用户列表',
+        icon:<UserOutlined />
+      }
+    ]
+  },
+  {
+    key:'/right-manage',
+    title:'权限管理',
+    icon:<UserOutlined />,
+    children:[
+      {
+        key:'/right-manage/role/list',
+        title:'角色列表',
+        icon:<UserOutlined />
+      },
+      {
+        key:'/right-manage/right/list',
+        title:'权限列表',
+        icon:<UserOutlined />
+      },
+    ]
+  },
+]
+function SideMenu(props) {
+//负责输出 item
+  const renderMenu=(menulist)=>{
+    return menulist.map(item=>{
+        if(item.children){
+          return getItem(item.title,item.key,item.icon,renderMenu(item.children))
+        }
+        return  getItem(item.title, item.key, item.icon,)
+      })
+  }
   return (
-    <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
+    <Sider trigger={null} collapsible collapsed={false}>
+      {/* collapsed={true}折叠属性 */}
+        <div className="logo">新闻发布系统</div>
+          <Menu 
+            theme="dark"  
+            mode="inline" 
+            defaultSelectedKeys={['1']}  
+            items={renderMenu(menuItem)}
+            onClick={(item)=>{props.history.push(item.key)}}
+          >
+          </Menu>
       </Sider>
   )
 }
+export default withRouter(SideMenu)
--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -8,10 +8,8 @@ export default function IndexRouter() {
         <Switch>
             <Route path='/login' component={Login}/>
             <Route path='/' render={()=>
-            localStorage.getItem('token')?
-            <NewsSandBox></NewsSandBox>:
-            <Redirect to='/login'/>
-            //权限鉴定 如果token验证正确,则进入新闻后台系统页面，否则重新进入登录界面
+              localStorage.getItem('token')?<NewsSandBox></NewsSandBox>:<Redirect to='/login'/>
+              //权限鉴定 如果token验证正确,则进入新闻后台系统页面，否则重新进入登录界面
         }/>
         </Switch>
       </HashRouter>
