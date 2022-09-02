ea4eb9cf40717cd646399eb666f0de917caab84f
13180599752@163.com <13180599752@163.com>
2022-09-02 20:19:10 +0800

    用户权限管理

--- src/views/sandbox/right-manage/RightList.js
+++ src/views/sandbox/right-manage/RightList.js
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Table } from 'antd'
 export default function RightList() {
+  const [state,setstate]=useState(
+    {
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号',
+    },
+    {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号',
+    },
+  )
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
   return (
     <div>
-      RightList
+      <Table dataSource={dataSource} columns={columns} />
     </div>
   )
 }
