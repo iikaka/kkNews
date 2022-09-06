9f600a548fc8e0cef0565b7e989a82332964c8b7
13180599752@163.com <13180599752@163.com>
2022-09-06 14:25:25 +0800

    角色列表

--- src/components/Sandbox/SideMenu.js
+++ src/components/Sandbox/SideMenu.js
@@ -67,7 +67,7 @@ function SideMenu(props) {
   //获取数据
   useEffect(()=>{
     axios.get('http://localhost:5000/rights?_embed=children').then(res=>{
-      console.log(res.data)
+      // console.log(res.data)
       setMenu(res.data)
     })
   },[])
--- src/views/sandbox/right-manage/RoleList.js
+++ src/views/sandbox/right-manage/RoleList.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React, { useState,useEffect } from 'react'
+import {Table} from 'antd'
+import axios from 'axios'
 export default function RoleList() {
+  const [dataSource,setdataSource] = useState([])
+  const columns=[]
+  useEffect(()=>{
+    axios.get('http://localhost:5000/roles').then(res=>{
+      console.log(res.data)
+    })
+  },[])
   return (
     <div>
-      RoleList
+      <Table dataSource={dataSource} columns={columns}></Table>
     </div>
   )
 }
